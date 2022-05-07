31a548e29a77941b23bbfffb18166cb90aeea63d
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-05-06 23:11:51 -0300

    aula 50 23:11

---
+++ nullUndefined.js
@@ -0,0 +1,15 @@
+let valor;
+
+console.log(valor);
+
+valor=null;
+
+console.log(valor);
+
+//console.log(valor.toString());//erro
+
+const produto = {};
+produto.nome='paozi';
+produto.preco=null;
+console.log(produto);
+
