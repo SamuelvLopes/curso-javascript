84eb148b757195fce0aef7bcd440276d597e39a5
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-05-06 22:53:12 -0300

    aula 47 fim 22:53

---
+++ objeto.js
@@ -0,0 +1,11 @@
+const animal = {};
+animal.especie = "taurus chifudis";
+animal['nome popular']='boi';
+animal.cientifico=[];
+animal.cientifico.nome='samuel';
+animal.cientifico.sobrenome='lopes';
+animal.cientifico.cor={};
+animal.cientifico.cor.pele="negro";
+animal.cientifico.cor.cabelo='ruivo';
+console.log(animal);
+console.log(animal.especie)
