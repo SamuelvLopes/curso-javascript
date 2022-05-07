bda3ad7935213c6d1f37b2329bf34f0f3cb9bdf4
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-05-06 22:43:44 -0300

    aula 47 22:43

---
+++ array.js
@@ -0,0 +1,11 @@
+const aa= ['2',2,'d',2,2,4,6,8,5,6,3];
+const b=[2];
+console.log(aa[0]);
+aa[99]=2;
+console.log(aa);
+console.log(aa.length);
+console.log(b.length);
+ console.log(b.pop());
+ console.log(b);
+
+console.log(typeof aa);
