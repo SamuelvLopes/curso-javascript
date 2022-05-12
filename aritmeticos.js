c2a627e5e08ab628797a16a889c1974cd77beb02
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-05-12 00:43:05 -0300

    aula 69

---
+++ aritmeticos.js
@@ -0,0 +1,9 @@
+const [a, b, c ,d, e] = [1, 2, 3, 4, 5];
+
+const soma = a+b+c+d+e;
+const subtracacao=a-b-c-d-e;
+const multiplicacao=a*b*c*d*e;
+const divisao = a/b/c/d/e;
+const modulo = a % 2;
+
+console.log(soma,subtracacao,multiplicacao,divisao,modulo);
