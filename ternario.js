f82aa33bae48a407a2feac1a6a3280c32c476dce
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-05-12 01:05:59 -0300

    aula 72

---
+++ ternario.js
@@ -0,0 +1,8 @@
+const resultado = nota => nota >=6 ? 'aprovado' : 'reprovado';
+
+console.log(resultado(2));
+
+console.log(resultado(8));
+const nota =6;
+
+nota>=5?console.log('menor ou igual 5'):console.log('menor que 5');
