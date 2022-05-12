89bf70b14b180cd747d6460e0c17fdf7584c7020
Samuel Lopes <samuelvitorprofissional@gmail.com>
2022-05-12 13:38:02 -0300

    aula 74

---
+++ fundamentos/try.js
@@ -0,0 +1,27 @@
+function tratarerro(erro){
+
+    //throw new Error(erro);
+    //throw 10;
+    //throw true;
+    throw{
+     nome:erro.name,
+     msg:erro.message,
+     date:new Date
+
+    }
+
+}
+function imprimirNomeGritado(obj){
+try{
+    console.log(obj.name.toUpperCase()+"!!!!");
+}catch(e){
+    tratarerro(e);
+
+}finally{
+
+    console.log('final');
+}
+}
+// certo const obj = {name:'samuel' }
+const obj = {nome:'samuel' }
+imprimirNomeGritado(obj)
